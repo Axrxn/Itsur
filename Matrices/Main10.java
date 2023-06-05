002c8ee8dff1bd77d5510a3a1a74aeca3cac93cd
Axrxn <132534576+Axrxn@users.noreply.github.com>
2023-06-05 16:53:24 -0600

    Matriz Transpuesta UP

---
+++ Matrices/Main10.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+   Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    int m = s.nextInt();
+    int[][] matriz = new int[n+2][m+2];
+    for(int i=0; i<n; i++){
+      for(int j=0; j<m; j++){
+        matriz[i][j] = s.nextInt();
+      }
+    }
+    for(int i=0; i<m; i++){
+      for(int j=0; j<n; j++){
+        System.out.print(matriz[j][i] + " ");
+      }
+      System.out.println();
+    }
+    
+  }
+}
