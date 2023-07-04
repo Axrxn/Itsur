1b9a41b6c4c347b88963116fda22dbdaed3ac875
Axrxn <132534576+Axrxn@users.noreply.github.com>
2023-07-04 14:32:27 -0600

    Rombo

---
+++ POO/Figura/Main7.java
@@ -0,0 +1,28 @@
+public class Rombo extends Cuadrilatero {
+    private double diagonalMayor;
+    private double diagonalMenor;
+
+    public double getDiagonalMayor() {
+        return diagonalMayor;
+    }
+
+    public void setDiagonalMayor(double diagonalMayor) {
+        this.diagonalMayor = diagonalMayor;
+    }
+
+    public double getDiagonalMenor() {
+        return diagonalMenor;
+    }
+
+    public void setDiagonalMenor(double diagonalMenor) {
+        this.diagonalMenor = diagonalMenor;
+    }
+
+    @Override
+    public double calcularArea() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+    
+    
+}
