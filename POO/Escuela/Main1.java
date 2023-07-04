f2c2ebeedf4631fb340df7fe60738879a3def302
Axrxn <132534576+Axrxn@users.noreply.github.com>
2023-07-04 14:12:11 -0600

    Carrera

---
+++ POO/Escuela/Main1.java
@@ -0,0 +1,11 @@
+public class Carrera {
+      private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+}
