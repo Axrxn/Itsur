3cb4632a919e253ad726a540fd0f9c11cf8f3501
Axrxn <132534576+Axrxn@users.noreply.github.com>
2023-07-04 14:19:27 -0600

    PruebaSueldo

---
+++ POO/Escuela/Main10.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+
+
+public class PruebaSueldo {
+    public static void main(String[] args) {
+        Docente d =  new Docente("Alberto Medina", "alberto@gmail.com", 
+        LocalDate.now(), "D1", 40, 10);
+        Docente d2 =  new Docente("Mario Medina", "mario@gmail.com", 
+        LocalDate.now(), "D2", 40, 400);
+//        General e1 =  new General("Martin Perez", "x@gmail.com", 
+//        LocalDate.now(), 20, 10) 
+    }
+    
+}
